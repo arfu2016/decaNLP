8d572b6834e793299a17311fa1c641589e713c6a
deco <deco@cubee.com>
2018-08-06 10:46:52 +0800

    Update run_data_load.py

--- run_data_load.py
+++ run_data_load.py
@@ -7,11 +7,20 @@
 """
 import logging
 import logging.handlers
+import os
+import sys
 from pprint import pformat
 
-from data_squad import arguments
-from text import torchtext
-from util import get_splits
+try:
+    from data_squad import arguments
+    from text import torchtext
+    from util import get_splits
+except ImportError:
+    base_dir = os.path.dirname(__file__)
+    sys.path.append(base_dir)
+    from data_squad import arguments
+    from text import torchtext
+    from util import get_splits
 
 
 def initialize_logger(args, rank='data_squad.data_load'):
@@ -42,9 +51,6 @@ def prepare_data(args, field, logger):
     # train sets, validation sets
     for task in args.train_tasks:
         logger.info(f'Loading {task}')
-        # kwargs = {'test': None}
-        # kwargs['subsample'] = args.subsample
-        # kwargs['validation'] = None
         kwargs = {'test': None,
                   'subsample': args.subsample,
                   # 'subsample': 20000000
@@ -54,16 +60,12 @@ def prepare_data(args, field, logger):
         split = get_splits(args, task, FIELD, **kwargs)[0]
         # 取了tuple的第一个元素，只保留train_data，不要validation data
         # split = torchtext.datasets.generic.SQuAD.splits(fields=FIELD,
-        # root=args.data, **kwargs)
+        # root=args.data, **kwargs)[0]
         logger.info(f'{task} has {len(split)} training examples')
         logger.debug(type(split))
         train_sets.append(split)
 
-        logger.debug(args.vocab_tasks)
-
     logger.debug(train_sets)
-
-    # return FIELD, train_sets, val_sets
 
 
 def main():
@@ -71,7 +73,6 @@ def main():
     if args is None:
         return
 
-    # 给numpy and torch设定seed
     logger = initialize_logger(args)
     logger.info(f'Arguments:\n{pformat(vars(args))}')
     # 调用vars(args)的format函数，得到字符串？
@@ -81,7 +82,6 @@ def main():
 
     # field is None
     prepare_data(args, field, logger)
-    # field, train_sets, val_sets = prepare_data(args, field, logger)
 
 
 if __name__ == '__main__':
